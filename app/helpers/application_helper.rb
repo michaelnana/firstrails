2c5c61fcccc07cc09fb1fe681d0a46ba7494446f
Michael Nana <michaelnananzepa@gmail.com>
2013-03-14 20:29:20 -0400

    Added page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  
+  def fulltitle(page_title)
+    basetitle="New Website"
+    if page_title.empty?
+      basetitle
+    else
+    "#{basetitle} | #{page_title}"
+  end
+  end
 end
