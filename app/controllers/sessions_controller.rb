aa1ac13a0bde317251192f5087157e6ad599f122
Michael Nana <michaelnananzepa@gmail.com>
2013-03-18 16:04:20 -0400

    Buffed up the sign in page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,11 @@ class SessionsController < ApplicationController
   
   def create
     user=User.find_by_email(params[:session][:email].downcase)
-    if user
+    if user && user.authenticate(params[:session][:password])
       sign_in user
       redirect_to (root_path)
+     else
+       redirect_to(root_path)
     end
   end
   
